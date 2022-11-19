8724fcf37d9c186fce1f870eeb7cf209fec532fd
chris p <Christopherperez1820@gmail.com>
2022-11-18 22:51:39 -0500

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get('/pokemon', (req, res) =>{
     res.render("Index", {pokemon: pokemon})
 })
 
+app.get('/pokemon/:id', (req, res) => {
+    res.send(req.params.id);
+})
 
 
 // APP LISTENER 
