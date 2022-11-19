e9e18509162900d757562719162104263c2af9e2
chris p <Christopherperez1820@gmail.com>
2022-11-18 22:44:53 -0500

    I can view a list of all my pokemon in the browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,21 +5,24 @@ class Index extends React.Component {
         const { pokemon } = this.props
         return (
             <div>
+                <head>
+                    <title>Pokemon App</title>
+                </head>
                 <h1>See All The Pokemon!</h1>
                 <nav>
-                    <a href="/pokemon/new">Create new fruit</a>
+                    <a href="/pokemon/new">Create new pokemon</a>
                 </nav>
                 <ul>
-                    {pokemon.map((poke, i )=> {
+                    {pokemon.map((eachMon, i )=> {
                         return (
                             // <li key =  {i}> is making "Warning: Each child in a list should have a unique "key" prop." go away 
-                            <li key = {i}>
-                                <a href='/pokemon'>{poke.name}</a>
+                            <ul key = {i}>
+                                <a href={`/pokemon/${pokemon._id}`}>{eachMon.name[0].toUpperCase() + eachMon.name.slice(1)}</a>
                                 {/* <a style={{color: "red"}} href={`/fruits/${fruit._id}`}>{fruit.name} */}
                                 {/* </a> */}
                                 
                                 <br />
-                            </li>
+                            </ul>
                         )
                     })}
                 </ul>
