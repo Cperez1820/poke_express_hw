22a23e005b7a9c3dd3288e71dc46a798d037a1c1
chris p <Christopherperez1820@gmail.com>
2022-11-22 18:43:17 -0500

    Added dyanmic anchor tags to index.jsx

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+// require('dotenv').config()
 const express = require("express")
 const app = express()
 const PORT = 3000
 const pokemon = require("./models/pokemon")
 const reactViews = require("express-react-views")
+// const mongoose = require('mongoose')
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   mongoose.connection.once("open", ()=>{
+//     console.log("connected to mongo");
+//   })
 
 app.set("view engine", "jsx")
 app.engine("jsx", reactViews.createEngine())
@@ -17,9 +27,34 @@ app.get('/pokemon', (req, res) =>{
 })
 
 app.get('/pokemon/:id', (req, res) => {
-    res.send(req.params.id);
+    res.render("Show",pokemon [req.params.id]);
 })
 
+
+
+app.get("/pokemon/:id", (req, res) => {
+    eachMon.findById(req.params.id, (error, foundeachMon) => {
+        if (!error) {
+            res.status (200).render('pokemon/Show', {
+                eachmon: foundeachMon 
+            })
+        } else {
+            res.status(400).send (error)
+        }
+    })
+})
+
+
+
+// res.render('fruits/show', {
+//     fruit: fruits [req.params.indexOfFruit] 
+// })
+// })
+
+// For reference 
+// app.get('/fruits/:indexOfFruitsArray', (req, res) => {
+//     res.send(fruits[req.params.indexOfFruitsArray]);
+// });
 
 // APP LISTENER 
 app.listen(PORT, ()=>{
